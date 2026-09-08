a3b0e27fd1a9e248dc0e7b011f4cdb11ba0a5d0c
Sasikar <sasipudi@gmail.com>
2026-09-08 09:36:33 +0530

    Refresh prices on app open

--- android/app/src/main/java/com/sasikar/trading/MainActivity.kt
+++ android/app/src/main/java/com/sasikar/trading/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +16,18 @@ class MainActivity : Activity() {
         web.settings.setSupportZoom(false)
         web.loadUrl("https://sasikar.github.io/Trading/")
         setContentView(web)
+        // Force a price pull while app is in foreground (most reliable on OEMs)
+        Thread {
+            try {
+                MarketWidget.refreshAllBlocking(applicationContext)
+                runOnUiThread {
+                    Toast.makeText(this, "ATrader prices updated", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: Throwable) {
+                runOnUiThread {
+                    Toast.makeText(this, "Price update failed: ${e.message}", Toast.LENGTH_LONG).show()
+                }
+            }
+        }.start()
     }
 }
