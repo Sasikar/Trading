c45c06e0ff4d813f164734a7bcee9fc00fc750ce
Sasikar <sasipudi@gmail.com>
2026-09-08 09:16:40 +0530

    version 2.7-age

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.sasikar.trading"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "2.6-5min"
+        versionCode = 18
+        versionName = "2.7-age"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
